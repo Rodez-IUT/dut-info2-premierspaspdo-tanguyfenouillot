a4229e98c1db0dbd499ff5ed0e6f6dde289be59d
tanguyfenouillot <tanguy.fenouillot@iut-rodez.fr>
2019-12-04 12:10:45 +0100

    Hello World

---
+++ HelloWorld/hello_world.php
@@ -0,0 +1,16 @@
+<?php
+spl_autoload_extensions(".php");
+spl_autoload_register();
+use yasmf\DataSource;
+use yasmf\Router;
+// have to create a dummy "hello_world" database...
+$dataSource = new DataSource(
+    $host = 'localhost',
+    $port = '3306',
+    $db = 'hello_world',
+    $user = 'root',
+    $pass = 'root',
+    $charset = 'utf8mb4'
+);
+$router = new Router() ;
+$router->route($dataSource);
